091a4414d6e280fff44f2ddb291f0ddb7335ab5e
gziz <69287011+gziz@users.noreply.github.com>
2022-07-10 17:58:05 -0500

    -

--- src/apis/LocationManager.js
+++ src/apis/LocationManager.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 
 //const baseURL = "http://127.0.0.1:5000"
-const baseURL = process.env.BACK_URL
+const baseURL = "https://cengange.herokuapp.com"
 
 export default axios.create({
     baseURL,
