3d2ca9cd836b75b89c89f3c04cbb5ec48d30d73c
gziz <69287011+gziz@users.noreply.github.com>
2022-07-10 14:36:44 -0500

    Add heroku url

--- src/apis/LocationManager.js
+++ src/apis/LocationManager.js
@@ -3,8 +3,8 @@ import axios from "axios";
 // NODE_ENV #1
 
 
-const baseURL = "http://127.0.0.1:5000"
-
+//const baseURL = "http://127.0.0.1:5000"
+const baseURL = "https://cengange.herokuapp.com"
 
 export default axios.create({
     baseURL,
